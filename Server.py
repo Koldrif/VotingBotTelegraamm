4b63e953e4386944b6bbd5575e30eba5bf29f4d5
Koldrif <49287905+Koldrif@users.noreply.github.com>
2021-06-19 22:46:27 +0300

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,25 @@
+import telebot
+import pymysql
+import pymysql.cursors
+
+#инициализация
+TOKEN=  r"1801737865:AAGQroqS0QitNBRVmqP58TQLWtjrl57CP38"
+bot = telebot.TeleBot(token=TOKEN)
+dataBase_bot = pymysql.connect(
+                                host="remotemysql.com",
+                                user="5SbqamHdMU",
+                                password="RqcFw1HOoz",
+                                database="5SbqamHdMU",
+                                )
+cursor = dataBase_bot.cursor()
+
+#Запрос на получение всех пар из таблицы
+requestToGetAllPairs = "SELECT * FROM `dictionary_of_pairs`"
+cursor.execute(requestToGetAllPairs)
+
+AllPairs =  cursor.fetchall()
+
+
+for row in rows:
+    print("id: " , row[0] , " first_expr: " , row[1] , " second expr: " , row[2])
+
